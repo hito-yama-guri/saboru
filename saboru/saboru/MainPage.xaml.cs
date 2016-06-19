4157b73eaff11597d2793c577b0a4ceab480d687
yamaguri <hito.yama.guri@gmail.com>
2016-06-19 15:41:19 +0900

    借りのリストを参照して、サボる判定をするようにした

--- saboru/saboru/MainPage.xaml.cs
+++ saboru/saboru/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace saboru
     public partial class MainPage : ContentPage
     {
         DateTime currentDate;
+        List<DateTime> primeDate = new List<DateTime>();
 
         public MainPage()
         {
@@ -21,6 +22,9 @@ namespace saboru
 
             prevButton.Clicked += prevButton_Clicked;
             nextButton.Clicked += nextButton_Clicked;
+
+            primeDate.Add(DateTime.Now.Date);
+            primeDate.Add( DateTime.Now.Date.AddDays( -2 ) );
         }
 
         private void prevButton_Clicked( object sender, EventArgs e )
@@ -37,6 +41,25 @@ namespace saboru
         {
             currentDate = currentDate.AddDays( diff );
             dateText.Text = currentDate.ToString( "yyyy/MM/dd" );
+
+            bool result = false;
+            foreach( DateTime date in primeDate ) {
+                if( date == currentDate ) {
+                    result = true;
+                    break;
+                }
+            }
+
+            if( result == true ) {
+                resultText.Text = "素数な日です！\nサボりましょう！";
+            } else {
+                resultText.Text = "素数な日ではありません...。\n社畜しましょう。";
+            }
+        }
+
+        private void applyText()
+        {
+
         }
     }
 }
