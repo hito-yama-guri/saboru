46d39b8e3a8aa47cf503d39191bf1647cd96e951
yamaguri <hito.yama.guri@gmail.com>
2016-06-19 15:25:54 +0900

    ボタンに日付の加減算を追加した

--- saboru/saboru/MainPage.xaml.cs
+++ saboru/saboru/MainPage.xaml.cs
@@ -10,9 +10,33 @@ namespace saboru
 {
     public partial class MainPage : ContentPage
     {
+        DateTime date;
+
         public MainPage()
         {
             InitializeComponent();
+
+            date = DateTime.Now;
+            dateText.Text = date.ToString("yyyy/MM/dd");
+
+            prevButton.Clicked += prevButton_Clicked;
+            nextButton.Clicked += nextButton_Clicked;
+        }
+
+        private void prevButton_Clicked( object sender, EventArgs e )
+        {
+            applyDate( -1 );
+        }
+
+        private void nextButton_Clicked( object sender, EventArgs e )
+        {
+            applyDate( 1 );
+        }
+
+        private void applyDate( int diff )
+        {
+            date = date.AddDays( diff );
+            dateText.Text = date.ToString( "yyyy/MM/dd" );
         }
     }
 }
